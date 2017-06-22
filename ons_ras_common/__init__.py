33602b1e155119be6b957d6aed240eb5b54b7c27
Gareth Bult <gareth@bult.co.uk>
2017-06-22 12:18:15 +0100

    Increment version

--- ons_ras_common/__init__.py
+++ ons_ras_common/__init__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 from .ons_environment import ONSEnvironment
 
-__version__ = '0.1.84'
+__version__ = '0.1.85'
 
 
 if 'ons_env' not in globals():

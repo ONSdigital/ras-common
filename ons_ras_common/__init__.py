e5af48db2e29dbeed44dc89e6c7d04c046920d53
Gareth Bult <gareth@bult.co.uk>
2017-06-28 12:34:04 +0100

    Adjust host/port on UI

--- ons_ras_common/__init__.py
+++ ons_ras_common/__init__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 from .ons_environment import ONSEnvironment
 
-__version__ = '0.1.94'
+__version__ = '0.1.95'
 
 
 if 'ons_env' not in globals():

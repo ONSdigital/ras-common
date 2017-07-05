872eae24a24301a70de6c042b1459cdbbabefde5
Duncan Nixon <duncannixon@me.com>
2017-07-05 15:33:30 +0100

    Further changes to support was-frontstagen.

--- ons_ras_common/__init__.py
+++ ons_ras_common/__init__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 from .ons_environment import ONSEnvironment
 
-__version__ = '0.1.123'
+__version__ = '0.1.124'
 
 
 if 'ons_env' not in globals():

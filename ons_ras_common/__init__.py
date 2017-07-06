fc4e5d06cfa2c75757446055db2ef4733796efc0
Gareth Bult <gareth@bult.co.uk>
2017-07-06 12:06:34 +0100

    Fix for 201 return code

--- ons_ras_common/__init__.py
+++ ons_ras_common/__init__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 from .ons_environment import ONSEnvironment
 
-__version__ = '0.1.129'
+__version__ = '0.1.130'
 
 
 if 'ons_env' not in globals():

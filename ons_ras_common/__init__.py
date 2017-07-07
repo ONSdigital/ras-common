64dc4918db3eb70c233d2964ef113b3d1c6abadc
Gareth Bult <gareth@bult.co.uk>
2017-07-07 14:04:12 +0100

    Fix typo

--- ons_ras_common/__init__.py
+++ ons_ras_common/__init__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 from .ons_environment import ONSEnvironment
 
-__version__ = '0.1.136'
+__version__ = '0.1.137'
 
 
 if 'ons_env' not in globals():

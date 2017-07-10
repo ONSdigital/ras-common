a1c53b61ae7c001edbfb335e7dfb6894b48d7530
Gareth Bult <gareth@bult.co.uk>
2017-07-10 10:50:47 +0100

    Checkpoint

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 #                                                                            #
 ##############################################################################
 from ons_ras_common import ons_env
+from ons_ras_common.ons_decorators import jwt_session
 
 if __name__ == '__main__':
     ons_env.activate()
--- ons_ras_common/__init__.py
+++ ons_ras_common/__init__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 from .ons_environment import ONSEnvironment
 
-__version__ = '0.1.138'
+__version__ = '0.1.139'
 
 
 if 'ons_env' not in globals():

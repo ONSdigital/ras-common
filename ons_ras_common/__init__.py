4e8bf96a19e46fa90648424614e517f29670de5a
Gareth Bult <gareth@bult.co.uk>
2017-07-11 14:41:56 +0100

    Additional logging

--- ons_ras_common/__init__.py
+++ ons_ras_common/__init__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 from .ons_environment import ONSEnvironment
 
-__version__ = '0.1.164'
+__version__ = '0.1.166'
 
 
 if 'ons_env' not in globals():

adfac4028b9a04bc3cb948519092f6ad84c24c88
Gareth Bult <gareth@bult.co.uk>
2017-07-12 09:23:31 +0100

    Working on history

--- ons_ras_common/__init__.py
+++ ons_ras_common/__init__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 from .ons_environment import ONSEnvironment
 
-__version__ = '0.1.175'
+__version__ = '0.1.176'
 
 
 if 'ons_env' not in globals():

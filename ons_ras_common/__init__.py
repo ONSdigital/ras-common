f289e6b3ed7ceb5fbf9587a0ee71f7599e37f671
Gareth Bult <gareth@bult.co.uk>
2017-07-13 18:17:17 +0100

    Working on global

--- ons_ras_common/__init__.py
+++ ons_ras_common/__init__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 from .ons_environment import ONSEnvironment
 
-__version__ = '0.1.205'
+__version__ = '0.1.206'
 
 
 if 'ons_env' not in globals():

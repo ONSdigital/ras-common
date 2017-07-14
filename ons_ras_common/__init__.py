18c0772e1dacf84a18962d498620d75d730d4dc5
Gareth Bult <gareth@bult.co.uk>
2017-07-14 14:11:35 +0100

    Resequencing

--- ons_ras_common/__init__.py
+++ ons_ras_common/__init__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 from .ons_environment import ONSEnvironment
 
-__version__ = '0.1.218'
+__version__ = '0.1.219'
 
 
 if 'ons_env' not in globals():

1a9f766da5efde940d6c405fe15b5c01741afc3e
Gareth Bult <gareth@bult.co.uk>
2017-07-14 16:27:24 +0100

    Update requirements.txt

--- ons_ras_common/__init__.py
+++ ons_ras_common/__init__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 from .ons_environment import ONSEnvironment
 
-__version__ = '0.1.221'
+__version__ = '0.1.222'
 
 
 if 'ons_env' not in globals():

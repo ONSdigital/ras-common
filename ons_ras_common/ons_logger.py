c655e373a98ec80c49ee10354b48174ce7f5b210
Gareth Bult <gareth@bult.co.uk>
2017-07-24 13:12:43 +0100

    Looking at debug error

--- ons_ras_common/ons_logger.py
+++ ons_ras_common/ons_logger.py
@@ -18,6 +18,7 @@ import arrow
 from sys import _getframe
 from zope.interface import provider
 from twisted.logger import Logger, ILogObserver
+from json import dumps
 log = None
 
 
@@ -53,7 +54,13 @@ class ONSLogger(object):
             try:
                 log_level = event.get('log_level', '')
                 log_format = event.get('log_format', '')
-                log_msg = str(log_format) if type(log_format) is dict else log_format.format(**event)
+                log_msg = dumps(log_format) if type(log_format) is dict else log_format.format(**event)
+
+                if type(log_format) is dict:
+                    print(event)
+                    print(dir(event))
+
+
                 log_name = _getframe(4).f_globals['__name__']
                 log_line = _getframe(4).f_lineno
                 print('{}: [{}] {} @{}#{}'.format(

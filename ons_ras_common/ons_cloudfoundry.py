f63ecda503988440a609a748c52ca560aa074521
Gareth Bult <gareth@bult.co.uk>
2017-08-04 12:30:52 +0100

    Nearly there

--- ons_ras_common/ons_cloudfoundry.py
+++ ons_ras_common/ons_cloudfoundry.py
@@ -1,14 +1,22 @@
-from cfenv import AppEnv
+import re
+import cfenv
+
+RegexType = type(re.compile(''))
+
 
 class ONSCloudFoundry(object):
 
     def __init__(self, env):
 
-        self._env = env
-        self._cf_env = AppEnv()
-        self._database = self._cf_env.get_service(tags='database') if self.detected else None
+        #
+        #   Monkey patch cfenv so it can handle a list match
+        #
+        cfenv.match = my_match
 
-        self._host = self._cf_env.uris.split(':')[0] if self.detected else 'localhost'
+        self._env = env
+        self._cf_env = cfenv.AppEnv()
+        self._database = self._cf_env.get_service(tags='database') if self.detected else None
+        self._host = self._cf_env.uris[0].split(':') if self.detected else 'localhost'
         self._port = 443 if self.detected else 8080
         self._protocol = 'https' if self.detected else 'http'
 
@@ -17,7 +25,7 @@ class ONSCloudFoundry(object):
 
     @property
     def url(self):
-        return self._database.get_url()
+        return self._database.credentials['uri']
 
     @property
     def detected(self):
@@ -34,3 +42,17 @@ class ONSCloudFoundry(object):
     @property
     def protocol(self):
         return self._protocol
+
+def my_match(target, pattern):
+    """
+    This is a customised version of "match" that also handles matching based
+    on 'target' possibly being a list.
+    """
+
+    if target is None:
+        return False
+    if isinstance(pattern, RegexType):
+        return pattern.search(target)
+    if isinstance(target, list):
+        return pattern in target
+    return pattern == target

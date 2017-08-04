159dc72cff4f290d4b77e7a55d5b4aa7498f6cb2
Gareth Bult <gareth@bult.co.uk>
2017-08-04 11:18:05 +0100

    Tweaking

--- ons_ras_common/ons_cloudfoundry.py
+++ ons_ras_common/ons_cloudfoundry.py
@@ -8,6 +8,10 @@ class ONSCloudFoundry(object):
         self._cf_env = AppEnv()
         self._database = self._cf_env.get_service(tags='database') if self.detected else None
 
+        self._host = self._cf_env.uris.split(':')[0] if self.detected else 'localhost'
+        self._port = 443 if self.detected else 8080
+        self._protocol = 'https' if self.detected else 'http'
+
     def activate(self):
         pass
 
@@ -18,3 +22,15 @@ class ONSCloudFoundry(object):
     @property
     def detected(self):
         return self._cf_env.app
+
+    @property
+    def port(self):
+        return self._port
+
+    @property
+    def host(self):
+        return self._host
+
+    @property
+    def protocol(self):
+        return self._protocol
